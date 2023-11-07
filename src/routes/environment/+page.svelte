3e1e17da8ee19281cb6506e56cd1f4c7fed1767d
Matthew Turk <matthewturk@gmail.com>
2023-11-07 12:27:31 -0600

    Attempt

---
+++ src/routes/environment/+page.svelte
@@ -0,0 +1,20 @@
+<div class="w-full grid grid-cols-6 md:grid-cols-6 gap-4"> 
+    <div class="m-4 p-4">
+        <button type="button" class="btn variant-filled">YAML Upload</button>
+    </div>
+    <div class="m-4 p-4">
+        <button type="button" class="btn variant-filled">Recipe Upload</button>
+    </div>
+    <div class="m-4 p-4">
+        <button type="button" class="btn variant-filled">Scripts Upload</button>
+    </div>
+    <div class="m-4 p-4">
+        <button type="button" class="btn variant-filled">Workspace Upload</button>
+    </div>
+    <div class="m-4 p-4">
+        <button type="button" class="btn variant-filled">Execute</button>
+    </div>
+    <div class="m-4 p-4">
+        <button type="button" class="btn variant-filled">Download</button>
+    </div>
+</div>
